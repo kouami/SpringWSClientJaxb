2a75eeababb8b9a76b435d551609a644e89930a6
Emmanuel <akolly@gmail.com>
2014-02-23 16:05:43 -0600

    Spring WS Client with Jaxb2

---
+++ SpringWsClientJaxb/src/main/java/org/efoe/poc/springws/client/App.java
@@ -0,0 +1,13 @@
+package org.efoe.poc.springws.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
